90d87164f6b2dba53818c3c3045a4afe27c6fe29
John Rake <john.rake12@gmail.com>
2013-07-24 22:30:36 -0500

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Riottabs::Application.config.secret_token = 'a3e48f119b70c29936bc4fc005e8263f04baa6e93d9f039872c546e147c077a99acff0261884d9d1cf9c6383cd228bcba2469fb4405efae443db37ec5cfafdcb'
